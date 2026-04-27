9dda52a0076233402fc2a717dc6866af7bcf2dcf
AGBEZE GODWIN UJA <godwinagbeze2@gmail.com>
2026-04-27 12:56:54 +0100

    i worked on the input and some other stuffs

--- mobile/app/app/dashboard.jsx
+++ mobile/app/app/dashboard.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Text, Image, TouchableOpacity, StyleSheet} from "react-native";
 import Ionicons from '@expo/vector-icons/Ionicons';
@@ -7,6 +8,8 @@ import Button from "../components/button";
 
 
 const DashBoard = () => {
+    const [rcNumber, setRcNumber] = useState("");
+
     return (
         <SafeAreaView>
 
@@ -17,7 +20,7 @@ const DashBoard = () => {
                     style={{ width: 40, height: 40, marginRight: 10 }}
                     source={require("../assets/images/insurance.png")}
                     />
-                <Text style={{alignSelf:"center", fontFamily:"bold"}}>CAC FastTrack</Text>
+                <Text style={{alignSelf:"center", fontFamily:"bold", fontSize:15}}>CAC FastTrack</Text>
                 </View>
 
                 <TouchableOpacity>
@@ -37,41 +40,47 @@ const DashBoard = () => {
             >Stop struggling with BN/O6 forms. Our AI extracts your data, validates it, and generates your return — 100% compliant.
             </Text>
 
-            <Input style={{marginHorizontal:40, marginTop:20}}/>
+            <Input
+                title=" Enter your RC Number or BN "
+                type="numeric"
+                value={rcNumber}
+                onChangeText={setRcNumber}
+                style={{marginHorizontal:40, marginTop:20}}
+            />
 
             <Button title="Get Started " style={{width:"78%", backgroundColor:"#22A84A",}}/>
 
 
-            {/* Feature Icons */}
-<View style={{ flexDirection: "row", justifyContent: "space-between", marginHorizontal: 10, marginTop: 50 }}>
+                        {/* Feature Icons */}
+            <View style={{ flexDirection: "row", justifyContent: "space-between", marginHorizontal: 10, marginTop: 50 }}>
 
-    {/* AI Powered Icon */}
-    <View style={{ alignItems: "center", flex: 1 }}>
-        <Image style={{ width: 40, height: 40, tintColor: "#1A73E8" }} source={require("../assets/images/ai.png")} />
-        <Text style={{ fontFamily: "medium", textAlign: "center", fontSize: 11, marginTop: 5 }}>AI Powered Extraction</Text>
-    </View>
+                {/* AI Powered Icon */}
+                <View style={{ alignItems: "center", flex: 1 }}>
+                    <Image style={{ width: 40, height: 40, tintColor: "#1A73E8" }} source={require("../assets/images/ai.png")} />
+                    <Text style={{ fontFamily: "medium", textAlign: "center", fontSize: 11, marginTop: 5 }}>AI Powered Extraction</Text>
+                </View>
 
-    {/* 100% Compliant Icon */}
-    <View style={{ alignItems: "center", flex: 1 }}>
-        <Image style={{ width: 40, height: 40, }} source={require("../assets/images/policy.png")} />
-        <Text style={{ fontFamily: "medium", textAlign: "center", fontSize: 11, marginTop: 5 }}>100% Compliant</Text>
-    </View>
+                {/* 100% Compliant Icon */}
+                <View style={{ alignItems: "center", flex: 1 }}>
+                    <Image style={{ width: 40, height: 40, }} source={require("../assets/images/policy.png")} />
+                    <Text style={{ fontFamily: "medium", textAlign: "center", fontSize: 11, marginTop: 5 }}>100% Compliant</Text>
+                </View>
 
-    {/* Download File Icon */}
-    <View style={{ alignItems: "center", flex: 1 }}>
-        <Image style={{ width: 40, height: 40, tintColor: "#1A73E8" }} source={require("../assets/images/icons8padlock.png")} />
-        <Text style={{ fontFamily: "medium", textAlign: "center", fontSize: 11, marginTop: 5 }}>Download & File</Text>
-    </View>
+                {/* Download File Icon */}
+                <View style={{ alignItems: "center", flex: 1 }}>
+                    <Image style={{ width: 40, height: 40, tintColor: "#1A73E8" }} source={require("../assets/images/icons8padlock.png")} />
+                    <Text style={{ fontFamily: "medium", textAlign: "center", fontSize: 11, marginTop: 5 }}>Download & File</Text>
+                </View>
 
             </View>
 
             
 
             
-    <View style={styles.footer}>
-    <Text style={styles.footerText}>Trusted by 1,000+ Nigerian Businesses</Text>
-    <Text style={styles.footerSubText}>Fast. Accurate. Compliant.</Text>
-    </View>
+                <View style={styles.footer}>
+                <Text style={styles.footerText}>Trusted by 1,000+ Nigerian Businesses</Text>
+                <Text style={styles.footerSubText}>Fast. Accurate. Compliant.</Text>
+                </View>
 
         </SafeAreaView>
     )
