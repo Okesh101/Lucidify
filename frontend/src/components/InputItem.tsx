2a29489c1f4d8e21cd676b796c55edf00879ceaf
marvellous <marvellousolorunwa23@gmail.com>
2026-05-05 13:38:26 +0100

    Added new things

--- frontend/src/components/InputItem.tsx
+++ frontend/src/components/InputItem.tsx
@@ -4,10 +4,11 @@ interface ItemProp{
     placeholder: string,
     title?: string,
     errorMssg?: string
-    value?: string
+    value?: string | number
     handleChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
     labelStyle? :string
     fieldsetStyle?: string
+    minValue?: number
 }
 const InputItem = ({
   name,
@@ -18,13 +19,14 @@ const InputItem = ({
   handleChange,
   value,
   fieldsetStyle,
-  labelStyle
+  labelStyle,
+  minValue
 }: ItemProp) => {
 
   return (
     <fieldset className={fieldsetStyle ?? "flex flex-col"}>
         <label htmlFor={name} className={labelStyle ?? "text-sm text-gray-600 font-[ClashDisplay] font-bold tracking-wider"}>{title}</label>
-        <input type={type} placeholder={placeholder} className="w-full mt-1 h-12 px-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#16A34A] font-[Onest]" name={name} value={value} onChange={handleChange}/>
+        <input type={type} placeholder={placeholder} min={minValue} className="w-full mt-1 h-12 px-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#16A34A] font-[Onest]" name={name} value={value} onChange={handleChange}/>
 
       {errorMssg && <p className="text-red-500 text-sm mt-1.5 font-[DMMono]">{errorMssg}</p> }
     </fieldset>
